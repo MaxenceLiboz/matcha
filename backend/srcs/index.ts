a418c943f2ecf4f473fe0a7cfd35abf44827289c
Maxence Liboz <mliboz@student.42lyon.fr>
2025-05-07 12:50:36 +0200

    [Evo] Ajout du debug pour le backend

--- backend/srcs/index.ts
+++ backend/srcs/index.ts
@@ -4,10 +4,10 @@ require("dotenv").config();
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response) => {
- res.status(200).json("Hello from the server!!!");
+    res.status(200).json("Hello from the server!!!");
 });
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
- console.log(`App is listening on port`, PORT);
+    console.log(`App is listening on port`, PORT);
 });
