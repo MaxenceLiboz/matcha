7d4f1933fd11373a9d8087e7e324505b2103e60c
Maxence Liboz <mliboz@student.42lyon.fr>
2025-06-04 10:27:19 +0200

    [Feat] Prepare for the validation of a user

--- frontend/src/features/auth/hooks/useLoginUser.ts
+++ frontend/src/features/auth/hooks/useLoginUser.ts
@@ -1,7 +1,5 @@
-// ../hooks/useLoginUser.ts
-
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { LoginUserRequest } from "../types";
 
 export type LoginFormValues = LoginUserRequest;
@@ -9,6 +7,22 @@ export type LoginFormValues = LoginUserRequest;
 export const useLoginUser = () => {
   const navigate = useNavigate();
   const [serverError, setServerError] = useState<string | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  // Get the token to validate the user.
+  const location = useLocation();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    setToken(queryParams.get('j'));
+  }, [location]);
+
+  useEffect(() => {
+    if (token) {
+      console.log(token);
+    }
+  }, [token])
+  
 
   return { serverError };
 };
