4a507dc73bc23c64a3c60db4fdb8851c914714fa
Maxence Liboz <mliboz@student.42lyon.fr>
2025-06-06 13:03:27 +0200

    Remove unused commit

--- frontend/src/types/AuthContext.ts
+++ frontend/src/types/AuthContext.ts
@@ -3,7 +3,7 @@ import { User } from "./User";
 export interface AuthContextType {
 	user: User | null;
 	isAuthenticated: boolean;
-	isLoading: boolean; // To handle initial loading of user from storage
-	login: (userData: User, access_token: string, refresh_token: string) => void; // token is optional
+	isLoading: boolean;
+	login: (userData: User, access_token: string, refresh_token: string) => void;
 	logout: () => void;
 }
