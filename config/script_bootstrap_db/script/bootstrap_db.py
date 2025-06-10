9247fca05b8cff94fa563b71bac8cf998f645c9d
Anas Zanane <azanane@z2r5p8.42lyon.fr>
2025-06-10 18:30:11 +0200

    [Evo] End of the Profile table + Location table + Tag table + User_Tag table

--- config/script_bootstrap_db/script/bootstrap_db.py
+++ config/script_bootstrap_db/script/bootstrap_db.py
@@ -2,6 +2,7 @@ import mysql.connector
 from faker import Faker
 import random
 import os
+import time
 
 try:
 
@@ -37,44 +38,88 @@ try:
 
     mycursor.execute("SET FOREIGN_KEY_CHECKS = 1")
 
-    # FILLING THE USER TABLE
+    # GENERATE FAKE DATA
 
     fake = Faker()
-    
-    val = []
 
-    for _ in range(500):
+    val_user = []
+    val_profile = []
+    val_location = []
+    val_tag = []
+    val_user_tag = []
 
+    genders = ["Male", "Female", "Other"]
+    sexual_preferences = ["Heterosexual", "Homosexual", "Other"]
+    now = time.localtime()
+    f = '%Y-%m-%d %H:%M:%S'
+
+    tag_list = fake.get_words_list()
+
+    for i in range(500):
+
+        user_id = i + 1
+        
+        # USER TABLE
         email = fake.unique.email()
         username = fake.unique.user_name()
         last_name = fake.unique.last_name()
         first_name = fake.unique.first_name()
         password_hash = fake.unique.password()
 
-        val.append((email, username, last_name, first_name, password_hash))
+        val_user.append((email, username, last_name, first_name, password_hash))
 
-    sql = "INSERT INTO User (email, username, last_name, first_name, password_hash) VALUES  (%s, %s, %s, %s, %s)"
-
-    mycursor.executemany(sql, val)
-
-    # FILLING THE PROFILE TABLE
-
-    val.clear()
-    genders = ["Male", "Female", "Other"]
-    sexual_preferences = ["Heterosexual", "Homosexual", "Other"]
-
-    for i in range(500):
-
-        user_id = i + 1
+        # PROFILE TABLE
         age = random.randint(18, 80)
         gender = random.choice(genders)
         sexual_preference = random.choice(sexual_preferences)
+        frame_rating = random.randint(1, 10)
+        current_timestamp = time.strftime(f, now)
 
-        val.append((user_id, age, gender, sexual_preference))
+        val_profile.append((user_id, age, gender, sexual_preference, frame_rating, current_timestamp, current_timestamp))
 
-    sql = "INSERT INTO Profile (user_id, age, gender, sexual_preference) VALUES  (%d, %d, %s, %s)"
+        # LOCATION TABLE
+        location = fake.location_on_land()
+        latitude = location[0]
+        longitude = location[1]
+        city = location[2]
 
-    mycursor.executemany(sql, val)
+        val_location.append((user_id, latitude, longitude, city, current_timestamp, current_timestamp))
+
+        # TAG TABLE
+        tag_name = fake.unique.word()
+
+        val_tag.append((tag_name, current_timestamp, current_timestamp))
+
+        # USER_TAG TABLE
+        tag_tab = []
+        for n in range(5):
+            random_tag = random.randint(1, 500)
+
+            while random_tag in tag_tab:
+                random_tag += 1
+                if random_tag > 500:
+                    random_tag = 1
+            
+            tag_tab.append(random_tag)
+            val_user_tag.append((user_id, random_tag, current_timestamp, current_timestamp))
+                
+
+
+
+    
+    # FILLING TABLES
+
+    sql_user = "INSERT INTO User (email, username, last_name, first_name, password_hash) VALUES  (%s, %s, %s, %s, %s)"
+    sql_profile = "INSERT INTO Profile (user_id, age, gender, sexual_preference, frame_rating, created_at, updated_at) VALUES  (%s, %s, %s, %s, %s, %s, %s)"
+    sql_location = "INSERT INTO Location (user_id, latitude, longitude, city, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s)"
+    sql_tag = "INSERT INTO Tag (name, created_at, updated_at) VALUES (%s, %s, %s)"
+    sql_user_tag = "INSERT INTO User_Tag (user_id, tag_id, created_at, updated_at) VALUES (%s, %s, %s, %s)"
+
+    mycursor.executemany(sql_user, val_user)
+    mycursor.executemany(sql_profile, val_profile)
+    mycursor.executemany(sql_location, val_location)
+    mycursor.executemany(sql_tag, val_tag)
+    mycursor.executemany(sql_user_tag, val_user_tag)
 
     mydb.commit()
 
